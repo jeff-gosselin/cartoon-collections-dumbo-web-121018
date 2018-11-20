a6c3065d452017ef437fc7908be215bc57dfb0af
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-11-20 05:48:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,3 +22,14 @@ end
 #     end
 #   end
 # end
+
+def find_the_cheese(ingredients)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if ingredients.find(cheese)
+      return cheese
+     else
+       return nil
+    end
+  end
+end
