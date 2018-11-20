252f78ec15666b33e46d645854e1867eed4c47e5
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-11-20 05:29:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,7 @@ end
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
-    ingredients.include?(cheese)
+   if ingredients.include?(cheese)
+    
   end
 end
